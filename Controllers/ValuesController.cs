fffda204f0cd712f43ae3aaff62d093077d885ae
andres lopez <darkandres22@gmail.com>
2019-07-12 09:05:42 -0500

    creacion del proyecto

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.DAO;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            DataSet structure =  new DataSet();
+            StringBuilder plane = new StringBuilder("0000001");
+
+            SQLTransaction ejecutar = new SQLTransaction();
+            structure = ejecutar.GetStruct();
+
+            for (int i = 1; i < structure.Tables[0].Rows.Count; i++)
+            {
+                //var a = structure.Tables[0].Rows[i]["Orden"].ToString();
+
+                if (structure.Tables[0].Rows[i]["Fuente"].ToString() == "")
+                {
+                    plane.Append(structure.Tables[0].Rows[i]["ValorFijo"].ToString());
+                }                
+
+                if (structure.Tables[0].Rows[i]["Orden"].ToString() == "1")
+                    plane.AppendLine();
+                
+            }
+
+
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ DAO/SQLTransaction.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WebAPI.DAO
+{
+    public class SQLTransaction
+    {
+public static IConfiguration Configuration { get; set; }
+        public DataSet GetStruct()
+        {
+            
+        var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
+            DataSet result = new DataSet();
+            Configuration = builder.Build();
+            SqlConnection connection = new SqlConnection(Configuration.GetConnectionString("DefaultConnection"));
+            SqlCommand command = new SqlCommand();
+            SqlDataAdapter daData = new SqlDataAdapter();
+            command.Connection = connection;
+            command.CommandType = CommandType.StoredProcedure;
+            command.CommandText = "sp_select_struct";
+            try
+            {
+                daData.SelectCommand = command;
+                daData.Fill(result);
+                result.Tables[0].TableName = "Struct";
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            finally
+            {
+                command.Connection.Close();
+            }
+
+
+
+            return result;
+        }
+
+    }
+}
