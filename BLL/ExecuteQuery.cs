c28f454ee25f25b1bac6f4fd9ede98f3e372fca9
andres lopez <darkandres22@gmail.com>
2019-08-01 17:58:47 -0500

    creacion de xml para consulta

--- BLL/ExecuteQuery.cs
+++ BLL/ExecuteQuery.cs
@@ -10,10 +10,22 @@ namespace WebAPI.BLL
         public DataSet SendQueryWS(QueryWSSiesa query)
         {
             DataSet data = new DataSet();
-            string xml = "";
+            string xml = "<Consulta>";
+            xml += $"<NombreConexion>{query.ConectionName}</NombreConexion>";
+            xml += $"<IdCia>{query.Cia}</IdCia>";
+            xml += $"<IdProveedor>{query.Provider}</IdProveedor>";
+            xml += $"<IdConsulta>{query.NameQuery}</IdConsulta>";
+            xml += $"<Usuario>{query.User}</Usuario>";
+            xml += $"<Clave>{query.Pass}</Clave>";
+            xml += "<Parametros>";
+            for (int i = 0; i < query.Filters.Count; i++)
+            {                
+                xml += $"<IdCia>{}</IdCia>";
+            }
+            xml += "</Parametros>";
+            xml += "</Consulta>";
 
-
-            wsSIESA.WSUNOEESoap ws = new wsSIESA.WSUNOEESoapClient(wsSIESA.WSUNOEESoapClient.EndpointConfiguration.WSUNOEESoap);
+           wsSIESA.WSUNOEESoap ws = new wsSIESA.WSUNOEESoapClient(wsSIESA.WSUNOEESoapClient.EndpointConfiguration.WSUNOEESoap);
             //var a = ws.EjecutarConsultaXMLAsync().Result;
 
             return data;
--- Models/QueryWSSiesa.cs
+++ Models/QueryWSSiesa.cs
@@ -5,6 +5,7 @@ namespace WebAPI.Models
     using System.Collections.Generic;
     public class QueryWSSiesa
     {
+        public string ConectionName { get; set; }
         public string NameQuery { get; set; }
         public string Provider { get; set; }
         public string Cia { get; set; }
